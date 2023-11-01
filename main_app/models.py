5de61d00f728120a58edee34b24c6d967f127d6d
Tuan Nguyen <tuan.nat915@gmail.com>
2023-11-01 19:59:04 -0400

    Create Feeding Model, FeedingForm, add form to detail.html, and add MCDatepicker

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 
 
 # Create your models here.
+
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=50)
@@ -14,3 +16,15 @@ class Finch(models.Model):
 
     def get_absolute_url(self):
         return reverse("finch-detail", kwargs={"finch_id": self.id})
+
+
+MEALS = (("B", "Breakfast"), ("L", "Lunch"), ("D", "Dinner"))
+
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.get_meal_display()} on {self.date}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .form import FeedingForm
 
 
 # Create your views here.
@@ -31,7 +32,10 @@ def finch_index(request):
 
 def finch_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, "finches/detail.html", {"finch": finch})
+    feeding_form = FeedingForm()
+    return render(
+        request, "finches/detail.html", {"finch": finch, "feeding_form": feeding_form}
+    )
 
 
 def about(request):
