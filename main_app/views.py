40ce9f0b4e8507f7abfee997397a1be21297df88
Tuan Nguyen <tuan.nat915@gmail.com>
2023-11-01 14:41:23 -0400

    Create finch-detail  and styling

--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-class Finch:
-    def __init__(self, name, color, description, age):
-        self.name = name
-        self.color = color
-        self.description = description
-        self.age = age
-
-
-finches = [
-    Finch("lolo", "yellow", "Kinda rude", 1),
-    Finch("fancy", "white", "happy bird", 3),
-    Finch("sachi", "green", "angry bird", 2)
-]
-
-
-def finch_index(request):
-    return render(request, "finches/index.html", {"finches": finches})
+from .models import Finch
 
 
 # Create your views here.
+
+
 def home(request):
-    return HttpResponse("<h1>This is home page</h1>")
+    return render(request, "home.html")
+
+
+def finch_index(request):
+    finches = Finch.objects.all()
+    return render(request, "finches/index.html", {"finches": finches})
+
+
+def finch_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, "finches/detail.html", {"finch": finch})
 
 
 def about(request):
